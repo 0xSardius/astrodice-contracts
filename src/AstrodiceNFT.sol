3fcd8b14df9679ed511d34e020e155c70d3df2af
Sardius <0xsardius@gmail.com>
2024-06-14 19:43:34 -0400

    init contract refactor w/o chainlink

--- src/AstrodiceNFT.sol
+++ src/AstrodiceNFT.sol
@@ -7,21 +7,20 @@ import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBase.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
 
-contract AstrodiceNFT is ERC721, VRFConsumerBase, Ownable {
+contract AstrodiceNFT is ERC721, Ownable {
     using Strings for uint256;
 
-    bytes32 internal keyHash;
-    uint256 internal fee;
     uint256 public tokenCounter;
 
     struct Astrodice {
         string planet;
         string sign;
         string house;
-        string planetImage;
-        string signImage;
-        string houseImage;
+        // string planetImage;
+        // string signImage;
+        // string houseImage;
         string planetSymbol;
+        string signSymbol;
     }
 
     string[] public planets = ["Sun", "Moon", "Mercury", "Venus", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto" "North Node", "South Node"];
@@ -34,76 +33,42 @@ contract AstrodiceNFT is ERC721, VRFConsumerBase, Ownable {
     string[] public signSymbols = ["\u2648", "\u2649", "\u264A", "\u264B", "\u264C", "\u264D" "\u264E", "\u264F", "\u2650", "\u2651", "\u2652", "\u2653"];
 
     mapping(uint256 => Astrodice) public tokenIdToAstrodice;
-    mapping(bytes32 => address) public requestIdToSender;
 
-    event RequestedAstrodice(bytes32 indexed requestId, uint256 indexed tokenId);
-    event CreatedAstrodice(uint256 indexed tokenId, string planet, string sign, string house, string planetImage, string signImage, string houseImage);
+    event CreatedAstrodice(uint256 tokenId, string planet, string sign, string house, /* string planetImage, string signImage, string houseImage, */ string planetSymbol, string signSymbol);
 
-    constructor(
-        address _VRFCoordinator,
-        address _LinkToken,
-        bytes32 _keyHash,
-        uint256 _fee
-    ) VRFConsumerBase(_VRFCoordinator, _LinkToken) ERC721("AstrodiceNFT", "ADICE") {
-        keyHash = _keyHash;
-        fee = _fee;
+    constructor() ERC721("AstrodiceNFT", "ADICE") {
         tokenCounter = 0;
     }
 
-    function createAstrodiceNFT() public returns (bytes32) {
-        bytes32 requestId = requestRandomness(keyHash, fee);
-        requestIdToSender[requestId] = msg.sender;
-        uint256 newTokenId = tokenCounter;
-        emit RequestedAstrodice(requestId, newTokenId);
-        return requestId;
-    }
-
-    function fulfillRandomness(bytes32 requestId, uint256 randomNumber) internal override {
-        address nftOwner = requestIdToSender[requestId];
+    function createAstrodiceNFT() public {
         uint256 newTokenId = tokenCounter;
 
-        uint256 planetIndex = randomNumber % planets.length;
-        uint256 signIndex = (randomNumber / planets.length) % signs.length;
-        uint256 houseIndex = (randomNumber / (planets.length * signs.length)) % houses.length;
+        // Generate a base random number using block attributes and user input
+        uint256 baseRandomNumber = uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty, msg.sender, newTokenId)));
 
+        // Derive independent random indices for each attribute
+        uint256 planetIndex = uint256(keccak256(abi.encodePacked(baseRandomNumber, "planet"))) % planets.length;
+        uint256 signIndex = uint256(keccak256(abi.encodePacked(baseRandomNumber, "sign"))) % signs.length;
+        uint256 houseIndex = uint256(keccak256(abi.encodePacked(baseRandomNumber, "house"))) % houses.length;
+
+        // Create a new Astrodice struct with the randomly selected attributed
         Astrodice memory newAstrodice = Astrodice(
             planets[planetIndex],
             signs[signIndex],
             houses[houseIndex],
-            planetImages[planetIndex],
-            signImages[signIndex],
-            houseImages[houseIndex]
+            // planetImages[planetIndex],
+            // signImages[signIndex],
+            // houseImages[houseIndex],
+            planetSymbols[planetIndex],
+            signSymbols[signIndex]
         );
         
+        //Store the Astrodice struct and mint the NFT
         tokenIdToAstrodice[newTokenId] = newAstrodice;
-        
-        _safeMint(nftOwner, newTokenId);
-        emit CreatedAstrodice(newTokenId, newAstrodice.planet, newAstrodice.sign, newAstrodice.house, newAstrodice.planetImage, newAstrodice.signImage, newAstrodice.houseImage);
-        
+        _safeMint(msg.sender, newTokenId);
+        emit CreatedAstrodice(newTokenId, newAstrodice.planet, newAstrodice.sign, newAstrodice.house,  /* newAstrodice.planetImage, newAstrodice.signImage, newAstrodice.houseImage,*/ newAstrodice.planetSymbol, newAstrodice.signSymbol);
+
         tokenCounter += 1;
     }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
-
-        Astrodice memory astrodice = tokenIdToAstrodice[tokenId];
-        string memory json = Base64.encode(bytes(string(abi.encodePacked(
-            '{"name": "Astrodice #', tokenId.toString(), '", "description": "An Astrodice NFT", "attributes": [',
-            '{"trait_type": "Planet", "value": "', astrodice.planet, '"},',
-            '{"trait_type": "Sign", "value": "', astrodice.sign, '"},',
-            '{"trait_type": "House", "value": "', astrodice.house, '"}],',
-            '"image": "', generateImageURI(astrodice), '"}'
-        ))));
-        return string(abi.encodePacked("data:application/json;base64,", json));
-    }
-
-    function generateImageURI(Astrodice memory astrodice) internal pure returns (string memory) {
-        return string(abi.encodePacked("data:image/svg+xml;base64,", Base64.encode(bytes(abi.encodePacked(
-            '<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">',
-            '<image href="', astrodice.planetImage, '" x="0" y="0" width="100" height="100"/>',
-            '<image href="', astrodice.signImage, '" x="100" y="0" width="100" height="100"/>',
-            '<image href="', astrodice.houseImage, '" x="50" y="100" width="100" height="100"/>',
-            '</svg>'
-        )))));
-    }
 }
